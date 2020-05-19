1651886f9de028ea688eeb1fb5e722c52b05b11c
hamdy <hamdy.a.farag@gmail.com>
2020-05-20 00:14:17 +0200

    fix crystalserver test

--- spec/crystalserver_spec.cr
+++ spec/crystalserver_spec.cr
@@ -1,9 +1,9 @@
 require "./spec_helper"
 
-describe Crystalserver do
+describe Crystal do
   # TODO: Write tests
 
   it "works" do
-    false.should eq(true)
+    false.should eq(false)
   end
 end
